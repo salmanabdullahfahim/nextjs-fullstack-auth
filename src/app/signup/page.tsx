19541317739dcf0f9befd44e8dfce32d90c26678
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-18 10:31:58 +0600

    Login page UI completed

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -10,9 +10,45 @@ const SingUpPage = () => {
     password: "",
     username: "",
   });
+
+  const onSignup = async () => {};
   return (
-    <div>
-      <h1 className="text-2xl text-center">SignUP</h1>
+    <div className="min-h-screen">
+      <div className="border-2 border-blue-500 flex flex-col items-center justify-center m-12 p-2 rounded-lg">
+        <h1 className="text-2xl font-medium py-6">Signup</h1>
+        <hr />
+        <label htmlFor="username">username</label>
+        <input
+          className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 mb-4"
+          type="text"
+          id="usename"
+          value={user.username}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+          placeholder="username"
+        />
+        <label htmlFor="email">email</label>
+        <input
+          className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 mb-4"
+          type="text"
+          id="usename"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="email"
+        />
+        <label htmlFor="password">password</label>
+        <input
+          className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 mb-4"
+          type="password"
+          id="usename"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="password"
+        />
+        <button className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 mb-4">
+          SignUp Here
+        </button>
+        <Link href="/login">Go to Login Page</Link>
+      </div>
     </div>
   );
 };
